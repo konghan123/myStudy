a2eaae75e5dd09aad4ac15baf0df4d527d85b770
kong <rud123gks@naver.com>
2023-11-01 22:26:00 +0900

    20231101_springDB(데이터 crud)

--- spring/스프링DB1 소스/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ spring/스프링DB1 소스/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 @Slf4j
 public class MemberRepositoryV0 {
@@ -28,6 +29,75 @@ public class MemberRepositoryV0 {
         }
     }
 
+    public Member findById(String memberId) throws SQLException {
+        String sql = "SELECT * FROM MEMBER WHERE MEMBER_ID = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+
+            if(rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("MEMBER_ID"));
+                member.setMoney(rs.getInt("MONEY"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found member_id = " + memberId);
+            }
+        }catch(SQLException e){
+            log.error("error" , e) ;
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
+    }
+    public void update(String memberId, int money) throws SQLException{
+        String sql = "UPDATE MEMBER SET MONEY = ? WHERE MEMBER_ID = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+
+            pstmt.executeUpdate();
+
+        }catch(SQLException e) {
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    public void delete(String memberId) throws SQLException{
+        String sql = "DELETE FROM MEMBER WHERE MEMBER_ID = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            pstmt.executeUpdate();
+
+        }catch(SQLException e) {
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
     private void close(Connection con, Statement stmt, ResultSet rs) {
 
         if(rs != null) {
--- spring/스프링DB1 소스/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ spring/스프링DB1 소스/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,12 +1,16 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
-
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repositoryV0 = new MemberRepositoryV0();
@@ -15,6 +19,18 @@ class MemberRepositoryV0Test {
 
         Member member = new Member("memberV0", 10000);
         repositoryV0.save(member);
+
+        Member findMember = repositoryV0.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
+
+        repositoryV0.update(member.getMemberId(), 12000);
+        Member updateMember = repositoryV0.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(12000);
+
+        repositoryV0.delete(member.getMemberId());
+        assertThatThrownBy( () -> repositoryV0.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
 
 
     }

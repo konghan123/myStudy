cd94af4273d6bbb4ba893a7ceb648458e121308f
kong <rud123gks@naver.com>
2023-11-07 21:02:14 +0900

    20231107_springDB(커넥션풀사용)

--- spring/스프링DB1 소스/jdbc/src/test/java/hello/jdbc/repository/connection/ConnectionTest.java
+++ spring/스프링DB1 소스/jdbc/src/test/java/hello/jdbc/repository/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.repository.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import hello.jdbc.connection.ConnectionConst;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -28,6 +29,25 @@ public class ConnectionTest {
         //DriverManagerDataSource - 항상 새로운 커넥션을 획득
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         userDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        userDataSource(dataSource);
+        Thread.sleep(1000);
+        // 커넥션 풀의 경우 애플리케이션 작업에 영향을 주는 것을 막기 위해
+        // 별도의 쓰레드를 할당하여 커넥션을 생성하고 있다 그래서 테스트에서는 확인이 불가능하기에
+        // 쓰레드를 한번 재워줌
+
+
     }
 
     private void userDataSource(DataSource dataSource) throws  SQLException {

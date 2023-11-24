53507d8551f55400b4a69d2843a1ff2985b83dac
kong <rud123gks@naver.com>
2023-11-24 22:21:28 +0900

    20231124_springDB(테스트_@Transactional)

--- spring/스프링DB2 소스/itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring/스프링DB2 소스/itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 
 //@Import(MemoryConfig.class)
@@ -25,5 +28,19 @@ public class ItemServiceApplication {
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
 	}
+
+//	@Bean
+//	@Profile("test")
+//	public DataSource dataSource() {
+//		//메모리 데이터베이스 초기화
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.h2.Driver");
+//		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+//		// jdbc:h2:mem:db -> 임베디드 모드로 작동
+//		// DB_CLOSE_DELAY= -1 -> 커넥션이 모두 끊어지면 데이터베이스도 종료되는데 그것을 방지
+//		dataSource.setUsername("sa");
+//		dataSource.setPassword("");
+//		return dataSource;
+//	}
 
 }

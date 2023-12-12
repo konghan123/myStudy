6802f0061874035627d971605f563d6a12f6914e
kong <rud123gks@naver.com>
2023-12-13 00:51:52 +0900

    20231213_spring핵심원리고급(프록시)

--- spring/스프링핵심개념-고급 소스/proxy/src/main/java/hello/proxy/app/v1/OrderControllerV1.java
+++ spring/스프링핵심개념-고급 소스/proxy/src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -1,12 +1,10 @@
 package hello.proxy.app.v1;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping//스프링은 @Controller 또는 @RequestMapping 이 있어야 스프링 컨트롤러로 인식
-@ResponseBody
+//@RequestMapping//스프링은 @Controller 또는 @RequestMapping 이 있어야 스프링 컨트롤러로 인식
+//@ResponseBody
+@RestController
 public interface OrderControllerV1 {
 
     @GetMapping("/v1/request")

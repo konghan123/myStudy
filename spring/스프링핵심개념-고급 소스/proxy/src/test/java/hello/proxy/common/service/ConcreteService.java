e60129783905494c24d708e0d04536130cbcd844
kong <rud123gks@naver.com>
2023-12-15 17:41:22 +0900

    20231215_spring핵심원리고급 (프록시 팩토리)

--- spring/스프링핵심개념-고급 소스/proxy/src/test/java/hello/proxy/common/service/ConcreteService.java
+++ spring/스프링핵심개념-고급 소스/proxy/src/test/java/hello/proxy/common/service/ConcreteService.java
@@ -3,7 +3,7 @@ package hello.proxy.common.service;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class ConcreteService {
+public class ConcreteService { // 인터페이스가 없는 구체 클래스
     public void call() {
         log.info("ConcreteService 호출");
     }
--- spring/스프링핵심개념-고급 소스/proxy/src/test/java/hello/proxy/common/service/ServiceImpl.java
+++ spring/스프링핵심개념-고급 소스/proxy/src/test/java/hello/proxy/common/service/ServiceImpl.java
@@ -3,7 +3,7 @@ package hello.proxy.common.service;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class ServiceImpl implements ServiceInterface {
+public class ServiceImpl implements ServiceInterface { //인터페이스가 있는 구체 클래스
     @Override
     public void save() {
         log.info("save 호출");
